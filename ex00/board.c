aeba2894564138d1d7e37c006235efa328f3427e
Georgijs Onischenko <xel27@inbox.lv>
2024-04-21 16:46:29 +0200

    added board.c file

---
+++ ex00/board.c
@@ -0,0 +1,60 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   board.c                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: gonische <gonische@student.42wolfsburg.    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/04/21 14:23:03 by gonische          #+#    #+#             */
+/*   Updated: 2024/04/21 16:43:44 by gonische         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdlib.h>
+#include <unistd.h>
+
+char	*create_board(size_t board_row_size)
+{
+	char			*result;
+	size_t			board_size;
+	unsigned int	i;
+
+	if (board_size > 9)
+		return (NULL);
+	board_size = board_row_size * board_row_size;
+	result = (char *)malloc(board_size);
+	i = 0;
+	while (i < board_size)
+	{
+		result[i] = (char)48;
+		i++;
+	}
+	return (result);
+}
+
+void	print_board(const char *board, size_t board_row_size)
+{
+	int		i;
+	size_t	board_size;
+
+	if (board_row_size > 9)
+		return ;
+	i = 0;
+	board_size = board_row_size * board_row_size;
+	while (i < board_size)
+	{
+		write(1, &board[i], 1);
+		i++;
+		if ((i % board_row_size) == 0)
+			write(1, "\n", 1);
+		else
+			write(1, " ", 1);
+	}
+	write(1, "\n", 1);
+}
+
+char	*get_board_cell(char *board, size_t board_row_size,
+		unsigned int row, unsigned int col)
+{
+	return (&board[(board_row_size * row) + col]);
+}
